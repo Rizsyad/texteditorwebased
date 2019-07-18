f381f2551522227bb52c5c8273b2b7e668173026
Rizsyad <42699914+Rizsyad@users.noreply.github.com>
2019-07-18 14:06:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Coding Online by INDOSEC">
+  	<meta name="keywords" content="coding,online,coding online,indosec">
+  	<meta name="author" content="Rizsyad AR">
+	<title>Coding Online | INDOSEC</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="row">
+	<div class="col-md-4 col-sm-12">
+		<div class="card">
+			<div class="card-header bg-dark text-white text-center">
+				HTML
+				<!-- <a href="#demo" data-toggle="collapse" class="text-white text-right">Collapsible</a> -->
+			</div>
+			<div class="table-responsive">
+				<div id="html" class="border" style="height: 300px;"></div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-12">
+		<div class="card">
+			<div class="card-header bg-dark text-white text-center">CSS</div>
+			<div class="table-responsive">
+				<div id="css" class="border" style="height: 300px;"></div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-12">
+		<div class="card">
+			<div class="card-header bg-dark text-white text-center">Javascript</div>
+			<div class="table-responsive">
+				<div id="javascript" class="border" style="height: 300px;"></div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12 col-sm-12">
+		<div class="card">
+			<div class="card-header bg-dark text-white text-center">VIEW</div>
+			<div class="embed-responsive embed-responsive-1by1">
+				<iframe class="embed-responsive-item" id="previewtarget" >
+				</iframe>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.5/ace.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+	$(document).ready(function() {
+		setEditor();
+		update();
+
+		function setEditor() {
+			window.htmleditor = ace.edit("html");
+			htmleditor.setTheme("ace/theme/monokai");
+			htmleditor.session.setMode("ace/mode/html");
+
+			htmleditor.getSession().on("change", function() {
+				update();
+			});
+
+			window.csseditor = ace.edit("css");
+			csseditor.setTheme("ace/theme/monokai");
+			csseditor.session.setMode("ace/mode/css");
+
+			csseditor.getSession().on("change", function() {
+				update();
+			});
+
+			window.javascripteditor = ace.edit("javascript");
+			javascripteditor.setTheme("ace/theme/monokai");
+			javascripteditor.session.setMode("ace/mode/javascript");
+
+			javascripteditor.getSession().on("change", function() {
+				update();
+			});
+
+			update();
+		}
+
+		function update() {
+			var result = document.getElementById("previewtarget").contentWindow.document;
+			result.open();
+			result.write(htmleditor.getValue()); 
+			result.write("<style>" + csseditor.getValue() + "<\/style>");
+			result.write("<script>" + javascripteditor.getValue() + "<\/script>");
+			result.close();
+		}
+
+	});
+</script>
+</body>
+</html>
